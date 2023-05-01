cbe290b0187caad6254239dcc304780e9e4b7331
Nnorom-coder <nnorom.prince44@gmail.com>
2023-05-01 22:55:24 +0100

    Added Enviroment var

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,9 @@ pip install telebot
 pip install python-dotenv
 pip install mysql-connector-python
 
+# Enviroment variables
+export BOTTOKEN =6010256354:AAHChAnylibLfKnJuxvM6wy--nRxEGIq4bw
+export SUCCESS_MESSAGE = 👍 Welcome To Mushee World
+
 # Run the bot
 python bot.py
