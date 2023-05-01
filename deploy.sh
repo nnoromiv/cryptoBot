e014eb54846a322bb1a1a053bccaa970750e1037
Nnorom-coder <nnorom.prince44@gmail.com>
2023-05-01 23:07:21 +0100

    shell update

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Create and activate a virtual environment
-python3 -m venv myenv
-source myenv/bin/activate
-
 # Install dependencies
 pip install --upgrade pip
 pip install telebot
 pip install python-dotenv
 pip install mysql-connector-python
+
+
+# Create and activate a virtual environment
+python -m venv myenv
+source myenv/bin/activate
 
 # Enviroment variables
 export BOTTOKEN =6010256354:AAHChAnylibLfKnJuxvM6wy--nRxEGIq4bw
