1dcfc42821b865456a52aa097d4d0583c7320076
Nnorom-coder <nnorom.prince44@gmail.com>
2023-05-01 23:58:36 +0100

    Removed VENu activation

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,6 @@ pip install telebot
 pip install python-dotenv
 pip install mysql-connector-python
 
-# Create and activate a virtual environment
-python -m venv myenv
-source myenv/bin/activate
-
 # Environment variables
 export BOTTOKEN="6010256354:AAHChAnylibLfKnJuxvM6wy--nRxEGIq4bw"
 export SUCCESS_MESSAGE="👍 Welcome To Mushee World"
